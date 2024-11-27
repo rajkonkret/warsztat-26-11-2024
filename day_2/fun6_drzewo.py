8756d56a77691bf51082503f3b62f407b37a026a
rajkonkret <rajkonkret660@gmail.com>
2024-11-27 11:09:40 +0100

    [2.8] add fun6

---
+++ day_2/fun6_drzewo.py
@@ -0,0 +1,24 @@
+import turtle
+
+# rysowanie żółwiem
+t = turtle.Turtle()
+t.speed(-1)  # najwyzsza szybkosc
+t.setheading(90)
+t.penup()
+t.goto(0, -200)
+t.pendown()
+
+
+def branch(t, len):
+    if len == 0: return
+    nt = t.clone()
+    nt.forward(50)
+    nt.left(20)
+    branch(nt, len - 1)
+    nt.right(40)
+    branch(nt, len - 1)
+
+
+branch(t, 7)
+window = turtle.Screen()
+window.exitonclick()
