7b33d1ad313ee39445fc7f8c7dc7ea63ecdd9379
rajkonkret <rajkonkret660@gmail.com>
2024-11-28 08:47:12 +0100

    [2.18] add fun17.py

--- day_2/fun_16_zad.py
+++ day_2/fun_16_zad.py
@@ -2,3 +2,59 @@
 # generator to wygenerowania danych
 # przetworzenie danych np.: filtrowanie danych
 # pomierzyc czas dla metody generującej raport
+import time
+
+
+def measure_time(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        execution_time = end_time - start_time
+        print(f"Czas wykonania funkcji {func.__name__}: {execution_time:.4f} sekundy")
+
+        return result
+
+    return wrapper
+
+
+def generator_danych(dane):
+    for element in dane:
+        yield element
+
+
+def przetworz_dane(dane):
+    return [element for element in dane if element % 3 == 0]
+
+
+@measure_time
+def stworz_raport(dane):
+    for element in generator_danych(dane):
+        print(f"Raport dla systemu {element}")
+
+
+dane = range(1_000_000)
+pr_dane = przetworz_dane(dane)
+stworz_raport(pr_dane)
+# Czas wykonania funkcji stworz_raport: 0.1291 sekundy
+# Raport dla systemu 999993
+# Raport dla systemu 999996
+# Raport dla systemu 999999
+
+# test Tabnine
+print("Koniec programu")
+print("Raport został wygenerowany")
+for _ in range(1000000):
+    suma = sum(range(1000000))
+    if suma == 500000500000:
+        break
+        print("Zakończono pętlę")
+        print("Suma wynosi 500000500000")
+
+
+def generator_danych_ze_slownika(slownik):
+    for key, value in slownik.items():
+        yield key, value
+
+
+slownik = {"Radek": 34, "Tomek": 56, "Zenek": 78, "Agnieszka": None}
