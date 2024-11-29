d147097981225de6fd42bdf82d1d0af17c40e4a6
rajkonkret <rajkonkret660@gmail.com>
2024-11-29 13:46:46 +0100

    [4.11]addd baza3.py

--- day_4/baza3.py
+++ day_4/baza3.py
@@ -37,3 +37,35 @@ class Address(Base):
 
 
 Base.metadata.create_all(engine)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+anakin = Person(name='Anakin', age=38)
+obi = Person(name="Obi Wan Kenobi", age=45)
+obi.addresses = [
+    Address(email='obi@example.com'),
+    Address(email='waaaka@example.com')
+]
+
+obi2 = Person(name="obi", age=54)
+
+session.add(anakin)
+session.add(obi)
+session.add(obi2)
+session.commit()
+
+all_ = session.query(Person).all()
+print(all_)
+# [Anakin (id=1), Obi Wan Kenobi (id=2), obi (id=3), Anakin (id=4), Obi Wan Kenobi (id=5), obi (id=6)]
+
+obi_list = session.query(Person).filter(
+    Person.name.like('Obi%')
+).all()
+
+for o in obi_list:
+    print(o)
+# obi (id=3)
+# Obi Wan Kenobi (id=5)
+# obi (id=6)
+# Obi Wan Kenobi (id=8)
+# obi (id=9)
